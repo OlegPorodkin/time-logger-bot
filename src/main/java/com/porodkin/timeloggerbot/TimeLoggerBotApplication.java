1296d76545c1a930d596a037576e7ba9a75b23a7
Oleg Porodkin <stixlive59@gmail.com>
2024-11-27 19:12:06 -0330

    Create project

---
+++ src/main/java/com/porodkin/timeloggerbot/TimeLoggerBotApplication.java
@@ -0,0 +1,11 @@
+package com.porodkin.timeloggerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLoggerBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeLoggerBotApplication.class, args);
+    }
+}
